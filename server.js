59fd0b25f2f37b875b8efd6d15923dda9d650f25
Vivek Jagtap <letsmailvivek1oo@gmailcom>
2026-05-07 14:54:46 +0530

    Fix root route redirect to login page

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-
 app.get("/", (req, res) => {
-  res.send("Someshwarnagar Food Order Server Running 🚀");
+  res.redirect("/login.html");
 });
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/test-db", (req, res) => {
   db.query("SELECT 1 + 1 AS result", (err, results) => {
